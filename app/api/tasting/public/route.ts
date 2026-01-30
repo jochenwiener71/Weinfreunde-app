a30e5d7d923b2bb96019d5ddd5c7301a276798b0
jochenwiener71 <jochenwiener71@gmail.com>
2026-01-30 13:58:20 +0100

    Update route.ts

--- app/api/tasting/public/route.ts
+++ app/api/tasting/public/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { db } from "../../../lib/firebaseAdmin";
+import { db } from "../../../../lib/firebaseAdmin";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
