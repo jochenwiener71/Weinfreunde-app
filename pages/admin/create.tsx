77ad01b5e0ca6ca594b3a0c62281bfb6f9b061cd
jochenwiener71 <jochenwiener71@gmail.com>
2026-01-30 12:57:36 +0100

    Update create.tsx

--- pages/admin/create.tsx
+++ pages/admin/create.tsx
@@ -1,1 +1,9 @@
-
+export default function AdminCreate() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Admin Create</h1>
+      <p>Wenn du das siehst, funktionieren UI-Routen ✅</p>
+      <p>Als nächstes bauen wir hier das Formular ein.</p>
+    </main>
+  );
+}
