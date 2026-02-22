ea546d44aefea4d7c1c21a45f994b87bdc5be047
jochenwiener71 <jochenwiener71@gmail.com>
2026-02-22 22:13:26 +0100

    Update page.tsx

--- app/admin/participants/[slug]/page.tsx
+++ app/admin/participants/[slug]/page.tsx
@@ -1,10 +1,6 @@
 import ParticipantsClient from "./ParticipantsClient";
 
-export default function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const slug = params?.slug ?? "";
+export default function Page({ params }: { params: { slug: string } }) {
+  const slug = decodeURIComponent(params.slug);
   return <ParticipantsClient slug={slug} />;
 }
