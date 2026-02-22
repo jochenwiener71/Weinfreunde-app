0970bb7d2d89ca051868d9c1614b00663c8ba32e
jochenwiener71 <jochenwiener71@gmail.com>
2026-02-22 21:39:32 +0100

    Update page.tsx

--- app/admin/tastings/[slug]/criteria/page.tsx
+++ app/admin/tastings/[slug]/criteria/page.tsx
@@ -1,1 +1,6 @@
+// app/admin/tastings/[slug]/criteria/page.tsx
+import CriteriaClient from "./CriteriaClient";
 
+export default function Page({ params }: { params: { slug: string } }) {
+  return <CriteriaClient slug={decodeURIComponent(params.slug)} />;
+}
